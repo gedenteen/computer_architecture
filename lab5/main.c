95b8e6c72ce659c0db1a025eb3172f0b6fd07502
gedenteen <razumow.dmitrij@yandex.ru>
2020-05-18 12:29:15 +0300

    Create functions ms_inteface_static() and ms_converte_write()

--- lab5/main.c
+++ lab5/main.c
@@ -28,16 +28,26 @@ int main()
     sc_regInit();
     mt_clrscr();
 
+    ram[0] = 10;
+    ram[11] = 3;
+    ram[22] = 28;
+    ram[33] = 33789;
+    ram[44] = 32768;
+    ram[55] = -289;
+    ram[66] = 7045;
+    instructionCounter = 255;
     bool quit = false;
+
+    ms_interface_static();
     while (quit == false)
     {
         ms_interface();
-            enum keys key = -1;
-            rk_readkey(&key);
-            if (key == KEY_Q)
-                quit = true;
-            else
-                ms_keyhandler(key);
+        enum keys key = -1;
+        rk_readkey(&key);
+        if (key == KEY_Q)
+            quit = true;
+        else
+            ms_keyhandler(key);
     }
     return 0;
 }
--- lab5/mySignals.h
+++ lab5/mySignals.h
@@ -9,6 +9,7 @@ int memx, memy;
 
 int ms_signalhandler();
 int ms_run();
+int ms_converte_write(int value, char *sign, int *command, int *operand);
 int ms_interface();
 int ms_keyhandler();
 
